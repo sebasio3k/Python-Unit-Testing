ce05f2c44d7ad560539ca5f59e4c0dd1a3afff24
SebastianHO <bastianhdezo@gmail.com>
2022-06-28 18:45:07 -0500

    clases, archivo de logs, config

---
+++ ingest.py
@@ -0,0 +1,14 @@
+import logging
+import logging.config
+
+
+class FileReader:
+    logging.config.fileConfig('./resources/configs/logging.conf')
+    logger = logging.getLogger('Ingest')
+
+    def __init__(self, file_type):
+        self.logger.info('I am within FileReader constructor')
+        self.file_type = file_type
+
+    def read_file(self):
+        self.logger.debug(f'Reading a {self.file_type} file ...')
